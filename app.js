943a3a0f9380221654e51080f75f33792582c249
Romulo <romulosas@gmail.com>
2019-06-05 08:30:17 -0400

    Using Vuew Form Validation

---
+++ app.js
@@ -0,0 +1,37 @@
+Vue.use(vuelidate.default);
+
+new Vue({
+  el: "#app",
+
+  data() {
+    return {
+      form: {
+        name: null,
+        age: null
+      }
+    };
+  },
+
+  validations: {
+    form: {
+      name: {
+        required: validators.required
+      },
+      age: {
+        required: validators.required,
+        integer: validators.integer,
+        between: validators.between(12, 120)
+      }
+    }
+  },
+
+  methods: {
+    submitForm() {
+      if (!this.$v.form.$invalid) {
+        console.log("Form Submitted", this.form);
+      } else {
+        console.log("Invalid form");
+      }
+    }
+  }
+});
